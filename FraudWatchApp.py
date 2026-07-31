b4bd11009094885da28163791b0c5e4b1d1efd1e
jaden-tan-np <S10268361@connect.np.edu.sg>
2026-07-31 13:38:37 +0800

    minor additions to the streamlit application

--- FraudWatchApp.py
+++ FraudWatchApp.py
@@ -50,7 +50,11 @@ while False: # test code
 
     st.text(pred)
 
-user_input = st.file_uploader("Upload transactions here:", type=[".csv"])
+
+st.title("FraudWatch Detection")
+
+
+user_input = st.file_uploader("Upload .csv file of transactions here:", type=[".csv"])
 
 
 if user_input:
@@ -78,3 +82,5 @@ if user_input:
     df_clean = df_clean[feature_names]
     df_clean = scaler.transform(df_clean)
     st.dataframe(xgb_model.predict(df_clean))
+
+    st.text("Total number of malicious transactions detected: {}".format(xgb_model.predict(df_clean).sum()))
